ef869295a00520cc4d1c8c95f96fccfbaf773c00
Kanek <dark_shadow921@hotmail.com>
2016-02-05 16:42:45 -0500

    Creacion de repo
    Creando el repo

---
+++ Deuda/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deudaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deudaApp
+ */
+angular.module('deudaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
